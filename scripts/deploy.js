2f0d310c24ecbd61ea8b1017287574ac54836522
Joe Marshal <joemarshal006@gmail.com>
2024-04-28 17:05:19 -0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
   const Voting = await ethers.getContractFactory("Voting");
 
   // Start deployment, returning a promise that resolves to a contract object
-  const Voting_ = await Voting.deploy(["Mark", "Mike", "Henry", "Rock"], 3);
+  const Voting_ = await Voting.deploy(["Joe", "Harshit", "Uday", "Dallas", "Khushi"], 10);
   console.log("Contract address:", Voting_.address);
 
 
